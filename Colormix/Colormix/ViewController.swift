79cd3bd1d2b478d322d9dcaf9e7115ca0f6012fe
ayberkyonar <302801376@student.rocmondriaan.nl>
2022-11-10 15:28:17 +0100

    color mixer beetje gemaakt

---
+++ Colormix/Colormix/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Colormix
+//
+//  Created by SD on 10/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        colorView.backgroundColor = .black
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor()
+    }
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+
+    }
+    
+}
+
---
+++ Colormix/ColormixUITests/ColormixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ColormixUITestsLaunchTests.swift
+//  ColormixUITests
+//
+//  Created by SD on 10/11/2022.
+//
+
+import XCTest
+
+class ColormixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

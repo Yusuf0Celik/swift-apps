ace526e8c7330645aff3122652b09cadfac90643
Yusuf0Celik <302801376@student.rocmondriaan.nl>
2023-01-19 13:05:02 +0100

    comments erin en score alert erin

--- Element Quiz/Element Quiz/ViewController.swift
+++ Element Quiz/Element Quiz/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
-
+// enum = private variable
+// Choose your playing mode
 enum Mode {
     case flashCard
     case quiz
 }
+// Get your private variable states
 enum State {
     case question
     case answer
@@ -18,14 +20,17 @@ enum State {
 }
 
 class ViewController: UIViewController, UITextFieldDelegate {
+    // Starting mode is flashcard
     var mode: Mode = .flashCard {
         didSet {
+            // if mode is flashCard/quiz do the following function
             switch mode {
             case .flashCard:
                 setupFlashCards()
             case .quiz:
                 setupQuiz()
             }
+            // Update the UI
             updateUI()
         }
     }
@@ -35,7 +40,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         mode = .flashCard
     }
-    
+    // Declare Objects
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var answerLabel: UITextField!
     @IBOutlet weak var textField: UITextField!
@@ -43,10 +48,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var showAnswerButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    // When pressing the showAnswerButton re
     @IBAction func showAnswer(_ sender: Any) {
         state = .answer
         updateUI()
     }
+    // When pressing the nextButton get next random element
     @IBAction func next(_ sender: Any) {
         currentElementIndex += 1
         if currentElementIndex >= elementList.count
@@ -61,6 +68,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         state = .question
         updateUI()
     }
+    
+    // When pressing modeSelector switch from modes
     @IBAction func switchModes(_ sender: Any) {
         if modeSelector.selectedSegmentIndex == 0 {
             mode = .flashCard
@@ -73,18 +82,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
     var answerIsCorrect = false
     var correctAnswerCount = 0
     
+    /// Make elementList
     let fixedElementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    /// Shuffle elementList
     var elementList: [String] = []
-    
     var currentElementIndex = 0
+    
     // Updates the app's UI in flash card mode.
     func updateFlashCardUI(elementName: String) {
+        // Hide textField and show Buttons
         // Text field and keyboard
         textField.isHidden = true
         textField.resignFirstResponder()
         // Buttons
         showAnswerButton.isHidden = false
         nextButton.isHidden = false
+        // When state is answer(showAnswer is clicked) nextButton is enabled
         switch state {
         case .question:
             nextButton.isEnabled = false
@@ -93,41 +106,50 @@ class ViewController: UIViewController, UITextFieldDelegate {
         case .score:
             nextButton.isEnabled = false
         }
-        // Answer label
+        // When state is answer Answer Label is the elementName
         if state == .answer {
             answerLabel.text = elementName
         } else {
+            // When state is not answer Answer Label is "?"
             answerLabel.text = "?"
         }
+        modeSelector.selectedSegmentIndex = 0
     }
 
     // Updates the app's UI in quiz mode.
     func updateQuizUI(elementName: String) {
+        // Show TextField, hide showAnswerButton and disable nextButton
         // Text field and keyboard
         textField.isHidden = false
         nextButton.isEnabled = false
         showAnswerButton.isHidden = true
+        // If Quiz is done show the score
         if currentElementIndex == elementList.count - 1 {
             nextButton.setTitle("Show Score", for: .normal)
         } else {
             nextButton.setTitle("Next Question", for: .normal)
         }
         switch state {
+        // If you need to answer the question textfield is enabled
         case .question:
             textField.isEnabled = true
             textField.text = ""
             textField.becomeFirstResponder()
+        // If the question is answered textField is disabled
         case .answer:
             textField.isEnabled = false
             textField.resignFirstResponder()
+        // If quiz is done textField is hidden
         case .score:
             textField.isHidden = true
             textField.resignFirstResponder()
         }
         // Answer label
         switch state {
+        // If you need to answer answerLable is empty
         case .question:
             answerLabel.text = ""
+        // If you need have answered nextButton is enabled and show wether you're right or wrong
         case .answer:
             if answerIsCorrect {
                 answerLabel.text = "Correct!"
@@ -136,16 +158,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 answerLabel.text = "❌"
                 nextButton.isEnabled = true
             }
+        // In the terminal it shows how many you got correct
         case .score:
             answerLabel.text = ""
             print("Your score is \(correctAnswerCount) out of \(elementList.count).")
         }
+        // If quiz ended it shows your score
+        if state == .score {
+            displayScoreAlert()
+        }
+        // If quiz ends you go back to flashcards
+        modeSelector.selectedSegmentIndex = 1
     }
     
     // Updates the app's UI based on its mode and
     func updateUI() {
-        let elementName =
-           elementList[currentElementIndex]
+        let elementName = elementList[currentElementIndex]
         let image = UIImage(named: elementName)
         imageView.image = image
         switch mode {
@@ -176,7 +204,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // Shows an iOS alert with the user's quiz score.
     func displayScoreAlert() {
         let alert = UIAlertController(title: "Quiz Score", message: "Your score is \(correctAnswerCount) out of \(elementList.count).", preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "OK", style: .default, handler: scoreAlertDismissed(_:))
+        let dismissAction = UIAlertAction(title: "OK", style: .default, handler:scoreAlertDismissed(_:))
         alert.addAction(dismissAction)
         present(alert, animated: true, completion: nil)
     }
